8644f92fca1682c017a0d7b20167aff025812be8
Stefano Dalpiaz <ste.dalpiaz@gmail.com>
2016-08-11 14:21:09 +1000

    Restored minification of bundle

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     }],
   },
   plugins: [
-    // new webpack.optimize.UglifyJsPlugin({ minimize: true }),
+    new webpack.optimize.UglifyJsPlugin({ minimize: true }),
   ],
   target: 'node',
 };
